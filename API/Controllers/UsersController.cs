a63bc73c3613b78fa73e09db627409d89f0b23d7
kennshi <edb.nikolaevich@gmail.com>
2024-11-29 15:24:43 +0200

    Added the authentication middleware

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,12 +1,15 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
+[Authorize]
 public class UsersController(DataContext context) : BaseApiController
 {
+    [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
@@ -15,6 +18,7 @@ public class UsersController(DataContext context) : BaseApiController
         return Ok(users);
     }
 
+    [Authorize]
     [HttpGet("{id:int}")] // /api/users/3
     public async Task<ActionResult<AppUser>> GetUser(int id)
     {
