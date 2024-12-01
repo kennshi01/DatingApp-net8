46462e84165958364b3f9bfa033f62a8d2fc3f84
kennshi <edb.nikolaevich@gmail.com>
2024-12-01 22:56:51 +0200

    Parent to child communication

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -13,13 +13,9 @@ import { HomeComponent } from "./home/home.component";
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-  http = inject(HttpClient);
   private accountService = inject(AccountsService);
-  title = 'DatingApp';
-  users: any;
 
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
   }
 
@@ -28,13 +24,5 @@ export class AppComponent implements OnInit {
     if(!userString) return;
     const user = JSON.parse(userString);
     this.accountService.currentUser.set(user);
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')
-    })
   }
 }
