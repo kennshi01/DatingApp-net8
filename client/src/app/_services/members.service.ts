b98efbd24a520c7b017ff18e98c966f2972d48bc
kennshi <edb.nikolaevich@gmail.com>
2024-12-25 18:17:36 +0200

    Using an interceptor to send the token

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,30 +1,20 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/member';
-import { AccountsService } from './accounts.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MembersService {
   private http = inject(HttpClient);
-  private accountService = inject(AccountsService)
   baseUrl = environment.apiUrl;
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users', this.getHttpOptions());
+    return this.http.get<Member[]>(this.baseUrl + 'users');
   }
 
   getMember(username: string) {
-    return this.http.get<Member>(this.baseUrl + 'users/' + username, this.getHttpOptions());
-  }
-
-  getHttpOptions() {
-    return {
-      headers: new HttpHeaders({
-        Authorization: `Bearer ${this.accountService.currentUser()?.token}`
-      })
-    }
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 }
