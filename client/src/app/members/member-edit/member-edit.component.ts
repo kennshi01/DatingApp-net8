530831cd45b9a72a06f5f35a5308d2503b711e8d
kennshi <edb.nikolaevich@gmail.com>
2024-12-30 15:19:13 +0200

    Updating User in Client App

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -38,8 +38,11 @@ export class MemberEditComponent implements OnInit {
   }
 
   updateMember() {
-    console.log(this.member);
-    this.toastr.success('Profile updated succesfuly');
-    this.editForm?.reset(this.member);
+    this.memberService.updateMember(this.editForm?.value).subscribe({
+      next: _ => {
+        this.toastr.success('Profile updated succesfuly');
+        this.editForm?.reset(this.member);        
+      }
+    })
   }
 }
