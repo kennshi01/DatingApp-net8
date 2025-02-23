f5fcb24496c0251f63f786b46aa1e75c176efef0
kennshi <edb.nikolaevich@gmail.com>
2025-02-24 00:29:55 +0200

    Implement pagination for user retrieval and add pagination headers

---
+++ API/Helpers/UserParams.cs
@@ -0,0 +1,13 @@
+namespace API.Helpers;
+
+public class UserParams
+{
+    private const int MaxPageSize = 50;
+    public int pageNumber { get; set; } = 1;
+    private int pageSize = 10;
+    public int PageSize
+    {
+        get => pageSize;
+        set => pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+    }
+}
